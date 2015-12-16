9be877caf0cdf0cf090891f4955ed51fe59b0ea1
Leo Yao <Leo Yao@Rice>
2015-12-16 10:31:43 -0500

    Base files

---
+++ desktop/src/com/battleforbronze/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.battleforbronze.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.battleforbronze.game.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
