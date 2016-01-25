80fe4183108c17125058e87b4ced589f396d4e70
valet8115 <valet8115@HRH-IC0027329.wrdsb.ca>
2016-01-25 11:24:04 -0500

    minion class added

--- core/src/com/battleforbronze/game/Screens/WorldRenderer.java
+++ core/src/com/battleforbronze/game/Screens/WorldRenderer.java
@@ -743,8 +743,9 @@ public class WorldRenderer {
                         if (temp.getTile().getId() != 186) {
                             temp.setTile(gameSet.getTile(188));
                         }
-                    } else if (temp.getTile().getId() == 187) {
+                     else if (temp.getTile().getId() == 187) {
                         temp.setTile(gameSet.getTile(205));
+                    }
                     }
                 }
                 RedTilesOnMapP1 = true;
@@ -758,15 +759,21 @@ public class WorldRenderer {
                         if (temp.getTile().getId() != 186) {
                             temp.setTile(gameSet.getTile(188));
                         }
-                    } else if (temp.getTile().getId() == 187) {
+                     else if (temp.getTile().getId() == 187) {
                         temp.setTile(gameSet.getTile(205));
+                    } else if (temp != null){
+                        System.out.println("yo");
                     }
-                    if (temp2 != null && temp.getTile().getId() != 187 && temp.getTile().getId() != 205) {
+                    }
+                    if (temp2 != null && temp2.getTile().getId() != 187 && temp2.getTile().getId() != 205) {
                         if (temp2.getTile().getId() != 186) {
                             temp2.setTile(gameSet.getTile(188));
                         }
-                    } else if (temp2.getTile().getId() == 187) {
+                     else if (temp2.getTile().getId() == 187) {
                         temp2.setTile(gameSet.getTile(205));
+                    } else if (temp != null){
+                        System.out.println("yo");
+                    }
                     }
                 }
                 RedTilesOnMapP1 = true;
